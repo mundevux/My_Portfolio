6bfc15bdb9dadfeb42aa7a998f0204674ceab452
Camilo Cuitiva <camiloandres174@gmail.com>
2022-08-22 21:19:48 -0500

    Creación de Proyecto

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    colors: {
+      primary: '#36393B',
+      secondary: '#E6E6E6',
+      tertiary: '#FB5012',
+      quaternary: '#2F97C1',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
